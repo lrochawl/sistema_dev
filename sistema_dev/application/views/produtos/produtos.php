174518f12522a9f8bc3773e6ea4bf1a6afbf70cf
Lucas Rocha <wltopos@gmail.com>
2023-03-24 08:54:01 -0300

    debug

--- sistema_dev/application/views/produtos/produtos.php
+++ sistema_dev/application/views/produtos/produtos.php
@@ -133,40 +133,46 @@
         </div>
         <div class="modal-body">
             <div class="span12 alert alert-info" style="margin-left: 0"> Escolha o intervalo de produtos para gerar as etiquetas.</div>
-
-            <div class="span12" style="margin-left: 0;">
             <div class="control-group">
-            <div class="controls">
-                <div class="span6" style="margin-left: 0;">
-                    <label for="valor">De</label>
-                    <input class="span9" style="margin-left: 0" type="text" id="de_id" name="de_id" placeholder="ID do primeiro produto" value="" />
+                <div class="span12" style="margin-left: 0;">
+                    <div class="control-group">
+                        <div class="controls">
+                            <div class="span6" style="margin-left: 0;">
+                                <label for="valor">De</label>
+                                <input class="span9" style="margin-left: 0" type="text" id="de_id" name="de_id" placeholder="ID do primeiro produto" value="" />
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="controls">
+                            <div class="span6">
+                                <label for="valor">Até</label>
+                                <input class="span9" type="text" id="ate_id" name="ate_id" placeholder="ID do último produto" value="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="controls">
+                            <div class="span4">
+                                <label for="valor">Qtd. do Estoque</label>
+                                <input class="span12" type="checkbox" name="qtdEtiqueta" value="true" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="span6">
+                        <label class="span12" for="valor">Formato Etiqueta</label>
+                        <select class="span5" name="etiquetaCode">
+                            <option value="EAN13">EAN-13</option>
+                            <option value="UPCA">UPCA</option>
+                            <option value="C93">CODE 93</option>
+                            <option value="C128A">CODE 128</option>
+                            <option value="CODABAR">CODABAR</option>
+                            <option value="QR">QR-CODE</option>
+                        </select>
+                    </div>
+
                 </div>
-
-
-                <div class="span6">
-                    <label for="valor">Até</label>
-                    <input class="span9" type="text" id="ate_id" name="ate_id" placeholder="ID do último produto" value="" />
-                </div>
-
-                <div class="span4">
-                    <label for="valor">Qtd. do Estoque</label>
-                    <input class="span12" type="checkbox" name="qtdEtiqueta" value="true" />
-                </div>
-
-                <div class="span6">
-                    <label class="span12" for="valor">Formato Etiqueta</label>
-                    <select class="span5" name="etiquetaCode">
-                        <option value="EAN13">EAN-13</option>
-                        <option value="UPCA">UPCA</option>
-                        <option value="C93">CODE 93</option>
-                        <option value="C128A">CODE 128</option>
-                        <option value="CODABAR">CODABAR</option>
-                        <option value="QR">QR-CODE</option>
-                    </select>
-                </div>
-
-            </div>
-        </div>
             </div>
         </div>
         <div class="modal-footer" style="display:flex;justify-content: center">
@@ -233,7 +239,7 @@
                 },
             ],
             "language": {
-               // processing: '<i class="fa fa-spinner fa-spin fa-3x fa-aw"></i>',
+                // processing: '<i class="fa fa-spinner fa-spin fa-3x fa-aw"></i>',
             },
             "responsive": true,
             "pagingType": $(window).width() < 768 ? 'simple' : 'numbers',
@@ -247,12 +253,12 @@
         //⬇️FUNÇÃO PARA INCLUIR VALORES DOS ATRIBUTOS DO LINK PARA VALUES DO MODAL
         $(document).on('click', 'a', function(event) {
 
-            let produto    = $(this).attr('produto');
-            let codigo     = $(this).attr('codigo');
+            let produto = $(this).attr('produto');
+            let codigo = $(this).attr('codigo');
             let estoqueTxt = $(this).attr('estoqueTxt');
-            let estoque    = $(this).attr('estoque');
+            let estoque = $(this).attr('estoque');
             let dataResult = $(this).attr('data_result');
-            let medida     = $(this).attr('medida');
+            let medida = $(this).attr('medida');
 
             if ($(this).attr('href') == "#modal-excluir") {
                 $('.idProduto').val(produto);
@@ -277,8 +283,8 @@
                     $('#selectMedida').append(`<option value="F">${dataR[2]}</option>`);
                 }
             }
-          
-             
+
+
 
         });
 
