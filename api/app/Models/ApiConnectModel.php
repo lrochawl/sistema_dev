88d8f8a89f863c38e12f692af391f717b4e6b7e9
Lucas Rocha <wltopos@gmail.com>
2023-03-25 20:50:20 -0300

    debug

--- api/app/Models/ApiConnectModel.php
+++ api/app/Models/ApiConnectModel.php
@@ -6,10 +6,7 @@ use CodeIgniter\Model;
 
 class ApiConnectModel extends Model
 {
-    protected $table      = 'usuarios';
-    protected $primaryKey = 'id';
-
-    protected $allowedFields = ['nome', 'email'];
+    
 
     public function getAPIData(){
         return $this->findAll();
