683a5dce544f1278d6877514050a0d68c759c99b
Lucas Rocha <wltopos@gmail.com>
2023-03-26 00:10:47 -0300

    debug

--- api/app/Controllers/Home.php
+++ api/app/Controllers/Home.php
@@ -16,8 +16,7 @@ class Home extends BaseController
 
         $apiConnectModel = new ApiConnectModel();
         $data['produtos'] = $apiConnectModel->getProdutoByLocal($local);
-print_r($data);
-exit();
+
         return view('welcome_message', $data);
     }
 }
