bb44124efaed79f0f1dc1795820fcb8815ff30c4
Lucas Rocha <wltopos@gmail.com>
2023-03-26 00:32:00 -0300

    debug

--- api/app/Views/qr_list.php
+++ api/app/Views/qr_list.php
@@ -17,12 +17,12 @@
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-md-6 text-center mb-4">
-					<h2 class="heading-section">Table #06</h2>
+					<h2 class="heading-section">Tabela de Produtos Localizados</h2>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-12">
-					<h3 class="h5 mb-4 text-center">Table Accordion</h3>
+					<h3 class="h5 mb-4 text-center">QR Search</h3>
 					<div class="table-wrap">
 						<table class="table">
 						  <thead class="thead-primary">
