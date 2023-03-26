cb6097e2f28aa5cca2c61c33930aa355edbf21da
Lucas Rocha <wltopos@gmail.com>
2023-03-25 22:14:29 -0300

    debug

--- api/app/Models/ApiConnectModel.php
+++ api/app/Models/ApiConnectModel.php
@@ -6,7 +6,10 @@ use CodeIgniter\Model;
 
 class ApiConnectModel extends Model
 {
-    
+    protected $table = 'estoque_produtos';
+    protected $primaryKey = 'id_estoque_produto';
+    protected $allowedFields = ['codDeBarra','produtoDescricao','estoque','estoqueMinimo','precoCompra','margemLucro','precoVenda','observacao','estoque_location_id','estoque_medida_id','estoque_marca_id','financeiro_nota_id','imagemProduto','pathImagem','saida','entrada','dataCadastro','dateUpdate','status','msg_status'];
+    protected $returnType = 'object';
     public function getAPIData(){
         return $this->findAll();
     }
