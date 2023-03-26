f15d1ec66b4dfab5a9234bfcddee2dc39bcb6259
Lucas Rocha <wltopos@gmail.com>
2023-03-25 23:02:52 -0300

    debug

--- api/app/Controllers/Home.php
+++ api/app/Controllers/Home.php
@@ -7,7 +7,12 @@ use App\Models\ApiConnectModel;
 class Home extends BaseController
 {
 
-    public function index($id)
+    public function index()
+    {
+        echo "ID do cliente: ". ;
+    }
+
+    public function mostrar($id)
     {
         echo "ID do cliente: ".$id ;
     }
