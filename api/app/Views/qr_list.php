24900d8398cf16a5f021767bde357d496dae9cd5
Lucas Rocha <wltopos@gmail.com>
2023-04-02 16:36:08 -0300

    debug

--- api/app/Views/qr_list.php
+++ api/app/Views/qr_list.php
@@ -75,7 +75,7 @@
 											<td><?= $produto->id_estoque_produto." - ". $produto->produtoDescricao ?></td>
 											<td class="quantity">
 												<div class="input-group">
-													<input type="text" name="quantity" class="quantity form-control input-number" value="<?= $produto->estoque ?>" min="1" max="100">
+													<span><?= $produto->estoque ?></span>
 												</div>
 											</td>
 											<td><?= $produto->tipo_produto ?></td>
