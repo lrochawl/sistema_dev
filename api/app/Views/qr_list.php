e54408c7b7f4286d414b3270384b1e0a6366c8ad
Lucas Rocha <wltopos@gmail.com>
2023-04-02 13:32:35 -0300

    corrige texto

--- api/app/Views/qr_list.php
+++ api/app/Views/qr_list.php
@@ -18,9 +18,9 @@
 			<div class="row justify-content-center">
 				<div class="col-md-6 text-center mb-4">
 					<?php if(is_array($produtos) and !empty($produtos)):?>
-					<h2 class="heading-section">Tabela de Produtos Localizados (<?= $local ?>)</h2>
+					<h2 class="heading-section">Tabela de Produtos Localizados | <?= $local ?></h2>
 					<?php else: ?>
-						<h2 class="heading-section">Não há produtos neste local (<?= $local ?>)</h2>
+						<h2 class="heading-section">Não há produtos neste local | <?= $local ?></h2>
 					<?php endif ?>
 				</div>
 			</div>
