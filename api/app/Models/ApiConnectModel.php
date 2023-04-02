4656a6008d4ac91b717498acefddcdad0e514327
Lucas Rocha <wltopos@gmail.com>
2023-04-02 18:30:02 -0300

    debug

--- api/app/Models/ApiConnectModel.php
+++ api/app/Models/ApiConnectModel.php
@@ -38,67 +38,64 @@ class ApiConnectModel extends Model
             ->where('id_estoque_medida', $idMedidaDefault)
             ->get();
 
-       // $data = get_object_vars($data);
-       $data =$data->getResult();
-print_r($data[0]->multiplicador);
-exit();
-
+       $data = get_object_vars($data);
+   
         if ($medidaConvert == 'D') { //Medida sistema para medida padrão
-            $estoque['valorConvertido'] = $estoqueAtual / $data['multiplicador'];
+            $estoque['valorConvertido'] = $estoqueAtual / $data[0]->multiplicador;
 
             if ($data['status'] == 2) {
-                $estoque['texto']   =  $estoque['valorConvertido'] . " " . ($estoque['valorConvertido'] > 1 ? $data['medida'] . 'S' : $data['medida']);
-                $estoque['textoRS'] = $estoque['valorConvertido'] . " " . ($estoque['valorConvertido'] > 1 ? $data['medida'] . 'S' : $data['medida']);
+                $estoque['texto']   =  $estoque['valorConvertido'] . " " . ($estoque['valorConvertido'] > 1 ? $data[0]->medida . 'S' : $data[0]->medida);
+                $estoque['textoRS'] = $estoque['valorConvertido'] . " " . ($estoque['valorConvertido'] > 1 ? $data[0]->medida . 'S' : $data[0]->medida);
 
-                $estoque['valorConvertidoEstoqueMinimo'] = $estoqueMinimo / $data['multiplicador'];
-                $estoque['textoEstoqueMinimo'] = $estoque['valorConvertidoEstoqueMinimo'] . " " . ($estoque['valorConvertido'] > 1 ? $data['medida'] . 'S' : $data['medida']);
-                $estoque['textoEstoqueMinimoRS'] = $estoque['valorConvertidoEstoqueMinimo'] . " " . ($estoque['valorConvertido'] > 1 ? $data['medida'] . 'S' : $data['medida']);
+                $estoque['valorConvertidoEstoqueMinimo'] = $estoqueMinimo / $data[0]->multiplicador;
+                $estoque['textoEstoqueMinimo'] = $estoque['valorConvertidoEstoqueMinimo'] . " " . ($estoque['valorConvertido'] > 1 ? $data[0]->medida . 'S' : $data[0]->medida);
+                $estoque['textoEstoqueMinimoRS'] = $estoque['valorConvertidoEstoqueMinimo'] . " " . ($estoque['valorConvertido'] > 1 ? $data[0]->medida . 'S' : $data[0]->medida);
             } else {
-                $estoque['texto'] = $estoque['valorConvertido'] . " " . ($estoque['valorConvertido'] > 1 ? $data['medida'] . 'S' : $data['medida']) . " COM " . $data['multiplicador'] . " " . ($data['multiplicador'] > 1 ? $data['medidaSistema'] . 'S' : $data['medidaSistema']);
-                $estoque['textoRS'] = $estoque['valorConvertido'] . " " . ($estoque['valorConvertido'] > 1 ? $data['medida'] . 'S' : $data['medida']);
+                $estoque['texto'] = $estoque['valorConvertido'] . " " . ($estoque['valorConvertido'] > 1 ? $data[0]->medida . 'S' : $data[0]->medida) . " COM " . $data[0]->multiplicador . " " . ($data[0]->multiplicador > 1 ? $data[0]->medidaSistema . 'S' : $data[0]->medidaSistema);
+                $estoque['textoRS'] = $estoque['valorConvertido'] . " " . ($estoque['valorConvertido'] > 1 ? $data[0]->medida . 'S' : $data[0]->medida);
 
-                $estoque['valorConvertidoEstoqueMinimo'] = $estoqueMinimo / $data['multiplicador'];
-                $estoque['textoEstoqueMinimo'] = $estoque['valorConvertidoEstoqueMinimo'] . " " . ($estoque['valorConvertido'] > 1 ? $data['medida'] . 'S' : $data['medida']) . " COM " . $data['multiplicador'] . " " . ($data['multiplicador'] > 1 ? $data['medidaSistema'] . 'S' : $data['medidaSistema']);
-                $estoque['textoEstoqueMinimoRS'] = $estoque['valorConvertidoEstoqueMinimo'] . " " . ($estoque['valorConvertido'] > 1 ? $data['medida'] . 'S' : $data['medida']);
+                $estoque['valorConvertidoEstoqueMinimo'] = $estoqueMinimo / $data[0]->multiplicador;
+                $estoque['textoEstoqueMinimo'] = $estoque['valorConvertidoEstoqueMinimo'] . " " . ($estoque['valorConvertido'] > 1 ? $data[0]->medida . 'S' : $data[0]->medida) . " COM " . $data[0]->multiplicador . " " . ($data[0]->multiplicador > 1 ? $data[0]->medidaSistema . 'S' : $data[0]->medidaSistema);
+                $estoque['textoEstoqueMinimoRS'] = $estoque['valorConvertidoEstoqueMinimo'] . " " . ($estoque['valorConvertido'] > 1 ? $data[0]->medida . 'S' : $data[0]->medida);
             }
 
-            $estoque['sigla'] = $data['siglaMedida'];
+            $estoque['sigla'] = $data[0]->siglaMedida;
 
             return $estoque;
         }
 
         if ($medidaConvert == 'S') { //Medida padrão para medida sistema
-            $estoque['valorConvertido'] = $estoqueAtual * $data['multiplicador'];
-            $estoque['texto'] = $estoque['valorConvertido'] . " " . ($estoque['valorConvertido'] > 1 ? $data['medidaSistema'] . 'S' : $data['medidaSistema']);
+            $estoque['valorConvertido'] = $estoqueAtual * $data[0]->multiplicador;
+            $estoque['texto'] = $estoque['valorConvertido'] . " " . ($estoque['valorConvertido'] > 1 ? $data[0]->medidaSistema . 'S' : $data[0]->medidaSistema);
 
-            $estoque['valorConvertidoEstoqueMinimo'] = $estoqueMinimo * $data['multiplicador'];
-            $estoque['textoEstoqueMinimo'] = $estoque['valorConvertido'] . " " . ($estoque['valorConvertido'] > 1 ? $data['medidaSistema'] . 'S' : $data['medidaSistema']);
+            $estoque['valorConvertidoEstoqueMinimo'] = $estoqueMinimo * $data[0]->multiplicador;
+            $estoque['textoEstoqueMinimo'] = $estoque['valorConvertido'] . " " . ($estoque['valorConvertido'] > 1 ? $data[0]->medidaSistema . 'S' : $data[0]->medidaSistema);
 
-            $estoque['sigla'] = $data['siglaMedidaSistema'];
+            $estoque['sigla'] = $data[0]->siglaMedidaSistema;
 
             return $estoque;
         }
 
         if ($medidaConvert == 'F') { //Medida sistema para medida fracionada
-            $estoque['valorConvertido'] = $estoqueAtual * $data['fracionadorSistema'];
-            $estoque['texto'] = $estoque['valorConvertido'] . " " . $data['fracionadorSistema'];
+            $estoque['valorConvertido'] = $estoqueAtual * $data[0]->fracionadorSistema;
+            $estoque['texto'] = $estoque['valorConvertido'] . " " . $data[0]->fracionadorSistema;
 
-            $estoque['valorConvertidoEstoqueMinimo'] = $estoqueAtual * $data['fracionadorSistema'];
-            $estoque['textoEstoqueMinimo'] = $estoque['valorConvertido'] . " " . $data['fracionadorSistema'];
+            $estoque['valorConvertidoEstoqueMinimo'] = $estoqueAtual * $data[0]->fracionadorSistema;
+            $estoque['textoEstoqueMinimo'] = $estoque['valorConvertido'] . " " . $data[0]->fracionadorSistema;
 
-            $estoque['sigla'] = $data['siglaFracaoSistema'];
+            $estoque['sigla'] = $data[0]->siglaFracaoSistema;
 
             return $estoque;
         }
 
         if ($medidaConvert == 'FF') { //Medida fracionada para medida sistema
-            $estoque['valorConvertido'] = $estoqueAtual / $data['fracionadorSistema'];
-            $estoque['texto'] = $estoque['valorConvertido'] . " " . $data['fracionadorSistema'];
+            $estoque['valorConvertido'] = $estoqueAtual / $data[0]->fracionadorSistema;
+            $estoque['texto'] = $estoque['valorConvertido'] . " " . $data[0]->fracionadorSistema;
 
-            $estoque['valorConvertidoEstoqueMinimo'] = $estoqueAtual * $data['fracionadorSistema'];
-            $estoque['textoEstoqueMinimo'] = $estoque['valorConvertido'] . " " . $data['fracionadorSistema'];
+            $estoque['valorConvertidoEstoqueMinimo'] = $estoqueAtual * $data[0]->fracionadorSistema;
+            $estoque['textoEstoqueMinimo'] = $estoque['valorConvertido'] . " " . $data[0]->fracionadorSistema;
 
-            $estoque['sigla'] = $data['siglaFracaoSistema'];
+            $estoque['sigla'] = $data[0]->siglaFracaoSistema;
 
             return $estoque;
         }
