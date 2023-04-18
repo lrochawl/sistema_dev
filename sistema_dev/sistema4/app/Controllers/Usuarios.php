cd29c3207f605b8ff23618f0cb89dacb5cd036ea
Lucas Rocha <wltopos@gmail.com>
2023-04-18 07:43:07 -0300

    debug

--- sistema_dev/sistema4/app/Controllers/Usuarios.php
+++ sistema_dev/sistema4/app/Controllers/Usuarios.php
@@ -55,7 +55,8 @@ class Usuarios extends BaseController
         }
 
         $retorno = ['data' => $data];
-
+print_r($this->response->setJSON($retorno));
+exit();
         return $this->response->setJSON($retorno);
         
     }
