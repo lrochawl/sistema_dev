d46df1646867ef78c49e830cd3d72ba4dc40dbca
Lucas Rocha <wltopos@gmail.com>
2023-04-19 18:40:32 -0300

    Update Usuarios.php

--- sistema_dev/sistema3/application/controllers/restrita/Usuarios.php
+++ sistema_dev/sistema3/application/controllers/restrita/Usuarios.php
@@ -7,7 +7,14 @@ class Usuarios extends CI_Controller{
     }
 
     public function index(){
+
+
+        $data = array(
+            'usuarios' => $this->ion_auth->users()->result(),
+        );
             
+        print_r($data);
+        exit();
         $this->load->view('restrita/layout/header');
         $this->load->view('restrita/usuarios/index');
         $this->load->view('restrita/layout/sidebar_settings');
