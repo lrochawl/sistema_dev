e4199e87f09b5480a6bd7f5a542070e39bfccc84
Lucas Rocha <wltopos@gmail.com>
2023-04-19 18:24:01 -0300

    Create index.php

---
+++ sistema_dev/sistema3/application/views/restrita/usuarios/index.php
@@ -0,0 +1,14 @@
+<?php $this->load->view('restrita/layout/navbar');?>
+<?php $this->load->view('restrita/layout/sidebar');?>
+
+            
+            <!-- Main Content -->
+            <div class="main-content">
+                <section class="section">
+                    <div class="section-body">
+                        <!-- add content here -->
+                    </div>
+                </section>
+                
+            </div>
+           
