a7a43d84c77d8692f4f0b0209c570cff26fceac3
Lucas Rocha <wltopos@gmail.com>
2023-04-19 19:54:59 -0300

    Update Usuarios.php

--- sistema_dev/sistema3/application/controllers/restrita/Usuarios.php
+++ sistema_dev/sistema3/application/controllers/restrita/Usuarios.php
@@ -16,7 +16,7 @@ class Usuarios extends CI_Controller{
                 'bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css',
             ),
             'scripts' => array(
-                'assets/bundles/datatables/datatables.min.js',
+                'bundles/datatables/datatables.min.js',
                 'bundles/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js',
                 'bundles/jquery-ui/jquery-ui.min.js',
                 'js/page/datatables.js'
