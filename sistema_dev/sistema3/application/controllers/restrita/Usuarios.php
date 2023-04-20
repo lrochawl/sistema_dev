1c2ac44b2ab79aa9bcd970a4bb233208321c73c2
Lucas Rocha <wltopos@gmail.com>
2023-04-20 15:00:31 -0300

    update

--- sistema_dev/sistema3/application/controllers/restrita/Usuarios.php
+++ sistema_dev/sistema3/application/controllers/restrita/Usuarios.php
@@ -53,7 +53,7 @@ class Usuarios extends CI_Controller
                 $this->form_validation->set_rules('first_name', 'Nome', 'trim|required|min_length[4]|max_length[45]');
                 $this->form_validation->set_rules('last_name', 'Sobrenome', 'trim|required|min_length[4]|max_length[45]');
                 $this->form_validation->set_rules('email', 'E-mail', 'trim|required|min_length[4]|max_length[100]|callback_valid_email|');
-                $this->form_validation->set_rules('')
+               
 
                 if ($this->form_validation->run()) {
                     echo '<pre>';
@@ -73,6 +73,33 @@ class Usuarios extends CI_Controller
                     $this->load->view('restrita/layout/footer');
                 }
             }
+        }
+    }
+
+    public function valida_email($email){
+
+        $usuario_id = $this->input->post('usuario_id');
+
+        if(!$usuario_id){
+            //Cadastrando
+
+            if($this->core_model->get_by_id('users', array('email' => $email))){
+                $this->form_validation->set_message('valida_email', 'Esse e-mail já existe');
+                return false;
+            }else{
+                return true;
+            }
+
+        }else{
+            //Editando
+
+            if($this->core_model->get_by_id('users', array('email' => $email, 'id !=' => $usuario_id))){
+                $this->form_validation->set_message('valida_email', 'Esse e-mail já existe');
+                return false;
+            }else{
+                return true;
+            }
+            
         }
     }
 }
