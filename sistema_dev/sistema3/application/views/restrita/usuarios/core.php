0e2990441d131b5305d464cdd7c3547d033a89d3
Lucas Rocha <wltopos@gmail.com>
2023-04-19 21:19:50 -0300

    Update core.php

--- sistema_dev/sistema3/application/views/restrita/usuarios/core.php
+++ sistema_dev/sistema3/application/views/restrita/usuarios/core.php
@@ -18,27 +18,28 @@ s<?php $this->load->view('restrita/layout/navbar'); ?>
 
               <div class="card-body">
                 <div class="form-row">
-                  <div class="form-group col-md-6">
+                  <div class="form-group col-md-4">
                     <label>Nome</label>
                     <input type="text" class="form-control" name="first_name">
                   </div>
-                  <div class="form-group col-md-6">
+                  <div class="form-group col-md-4">
                     <label>Sobrenome</label>
                     <input type="text" class="form-control" name="last_name">
                   </div>
-                  <div class="form-group col-md-6">
-                    <label for="inputEmail4">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                  <div class="form-group col-md-4">
+                    <label>E-mail</label>
+                    <input type="email" class="form-control" name="email">
                   </div>
-                  <div class="form-group col-md-6">
-                    <label for="inputPassword4">Password</label>
-                    <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-                  </div>
+                  
                 </div>
                 <div class="form-group">
                   <label for="inputAddress">Address</label>
                   <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
                 </div>
+                <div class="form-group col-md-6">
+                    <label for="inputPassword4">Password</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                  </div>
                 <div class="form-group">
                   <label for="inputAddress2">Address 2</label>
                   <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
