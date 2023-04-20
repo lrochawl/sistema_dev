84d7102304fa2b0bbf5885353f27df73edbba29b
Lucas Rocha <wltopos@gmail.com>
2023-04-19 21:38:34 -0300

    Update core.php

--- sistema_dev/sistema3/application/views/restrita/usuarios/core.php
+++ sistema_dev/sistema3/application/views/restrita/usuarios/core.php
@@ -13,26 +13,22 @@ s<?php $this->load->view('restrita/layout/navbar'); ?>
             <div class="card-header">
               <h4><?= isset($titulo) ? $titulo : 'EDITAR ---' ?></h4>
             </div>
+            <form method="post" name="form_core" action="">
             <div class="card-body">
-
-
-              <div class="card-body">
-                <div class="form-row">
-                  <div class="form-group col-md-4">
-                    <label>Nome</label>
-                    <input type="text" class="form-control" name="first_name">
-                  </div>
-                  <div class="form-group col-md-4">
-                    <label>Sobrenome</label>
-                    <input type="text" class="form-control" name="last_name">
-                  </div>
-                  <div class="form-group col-md-4">
-                    <label>E-mail</label>
-                    <input type="email" class="form-control" name="email">
-                  </div>
-                  
+              <div class="form-row">
+                <div class="form-group col-md-4">
+                  <label>Nome</label>
+                  <input type="text" class="form-control" name="first_name">
                 </div>
-
+                <div class="form-group col-md-4">
+                  <label>Sobrenome</label>
+                  <input type="text" class="form-control" name="last_name">
+                </div>
+                <div class="form-group col-md-4">
+                  <label>E-mail</label>
+                  <input type="email" class="form-control" name="email">
+                </div>
+              </div>
               <div class="card-body">
                 <div class="form-row">
                   <div class="form-group col-md-4">
@@ -47,40 +43,33 @@ s<?php $this->load->view('restrita/layout/navbar'); ?>
                     <label>Confirmar senha</label>
                     <input type="password" class="form-control" name="confirma">
                   </div>
-                  
                 </div>
-
                 <div class="form-row">
-
-
                   <div class="form-group col-md-4">
-                    <label >Situação</label>
+                    <label>Situação</label>
                     <select class="form-control" name="active">
-                      <option >Ativo</option>
-                      <option >Inativo</option>
+                      <option>Ativo</option>
+                      <option>Inativo</option>
                     </select>
                   </div>
                   <div class="form-group col-md-4">
-                    <label >Perfil de acesso</label>
+                    <label>Perfil de acesso</label>
                     <select class="form-control" name="group">
                       <option selected>Cliente</option>
                       <option>Administrador</option>
                     </select>
                   </div>
-
                 </div>
-               
-                
               </div>
               <div class="card-footer">
                 <button class="btn btn-primary">Submit</button>
               </div>
+</form>
             </div>
           </div>
         </div>
       </div>
     </div>
-</div>
-</section>
+  </section>
 
 </div>
