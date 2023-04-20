725e6f25d4d3ce1ec462c0d7e758b683b75ed4af
Lucas Rocha <wltopos@gmail.com>
2023-04-20 16:22:11 -0300

    Update index.php

--- sistema_dev/sistema3/application/views/restrita/usuarios/index.php
+++ sistema_dev/sistema3/application/views/restrita/usuarios/index.php
@@ -15,6 +15,18 @@ s<?php $this->load->view('restrita/layout/navbar');?>
                   </div>
                   <div class="card-body">
 
+                  <?php if($message = $this->session->flashdata('sucesso')):?>
+                    <div class="alert alert-success alert-has-icon alert-dismissible show fade">
+                        <div class="alert-icon"><i class="fas fa-check-circle fa-lg"></i></div>
+                        <div class="alert-body">
+                            <div class="alert-title">Sucesso</div>
+                            <button class="close" data-demiss="alert">
+                                <span>&times;</span>
+                            </button>
+                            <?= $message ?>
+                        </div>
+                    </div>
+                    <?php endif?>
                   <?php if($message = $this->session->flashdata('erro')):?>
                     <div class="alert alert-danger alert-has-icon alert-dismissible show fade">
                         <div class="alert-icon"><i class="fas fa-exclamation-circle fa-lg"></i></div>
