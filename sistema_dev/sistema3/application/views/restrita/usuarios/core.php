201d39344806ae648e4a7dbfac96d865efc035d9
Lucas Rocha <wltopos@gmail.com>
2023-04-19 21:10:39 -0300

    Update core.php

--- sistema_dev/sistema3/application/views/restrita/usuarios/core.php
+++ sistema_dev/sistema3/application/views/restrita/usuarios/core.php
@@ -1,81 +1,79 @@
-s<?php $this->load->view('restrita/layout/navbar');?>
-<?php $this->load->view('restrita/layout/sidebar');?>
+s<?php $this->load->view('restrita/layout/navbar'); ?>
+<?php $this->load->view('restrita/layout/sidebar'); ?>
 
-            
-            <!-- Main Content -->
-            <div class="main-content">
-                <section class="section">
-                    <div class="section-body">
-                        <!-- add content here -->
-                        <div class="row">
-              <div class="col-12">
-                <div class="card">
-                  <div class="card-header">
-                    <h4><?= isset($titulo)?$titulo:'EDITAR ---'?></h4>
-                  </div>
-                  <div class="card-body">
-                   
 
-                  <div class="card">
-                  <div class="card-header">
-                    <h4>Horizontal Form</h4>
+<!-- Main Content -->
+<div class="main-content">
+  <section class="section">
+    <div class="section-body">
+      <!-- add content here -->
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-header">
+              <h4><?= isset($titulo) ? $titulo : 'EDITAR ---' ?></h4>
+            </div>
+
+
+
+            <div class="card">
+              <div class="card-header">
+                <h4>Horizontal Form</h4>
+              </div>
+              <div class="card-body">
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="inputEmail4">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
                   </div>
-                  <div class="card-body">
-                    <div class="form-row">
-                      <div class="form-group col-md-6">
-                        <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-                      </div>
-                      <div class="form-group col-md-6">
-                        <label for="inputPassword4">Password</label>
-                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label for="inputAddress">Address</label>
-                      <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                    </div>
-                    <div class="form-group">
-                      <label for="inputAddress2">Address 2</label>
-                      <input type="text" class="form-control" id="inputAddress2"
-                        placeholder="Apartment, studio, or floor">
-                    </div>
-                    <div class="form-row">
-                      <div class="form-group col-md-6">
-                        <label for="inputCity">City</label>
-                        <input type="text" class="form-control" id="inputCity">
-                      </div>
-                      <div class="form-group col-md-4">
-                        <label for="inputState">State</label>
-                        <select id="inputState" class="form-control">
-                          <option selected>Choose...</option>
-                          <option>...</option>
-                        </select>
-                      </div>
-                      <div class="form-group col-md-2">
-                        <label for="inputZip">Zip</label>
-                        <input type="text" class="form-control" id="inputZip">
-                      </div>
-                    </div>
-                    <div class="form-group mb-0">
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">
-                          Check me out
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="card-footer">
-                    <button class="btn btn-primary">Submit</button>
+                  <div class="form-group col-md-6">
+                    <label for="inputPassword4">Password</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
                   </div>
                 </div>
+                <div class="form-group">
+                  <label for="inputAddress">Address</label>
+                  <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                </div>
+                <div class="form-group">
+                  <label for="inputAddress2">Address 2</label>
+                  <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="inputCity">City</label>
+                    <input type="text" class="form-control" id="inputCity">
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="inputState">State</label>
+                    <select id="inputState" class="form-control">
+                      <option selected>Choose...</option>
+                      <option>...</option>
+                    </select>
+                  </div>
+                  <div class="form-group col-md-2">
+                    <label for="inputZip">Zip</label>
+                    <input type="text" class="form-control" id="inputZip">
+                  </div>
+                </div>
+                <div class="form-group mb-0">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">
+                      Check me out
+                    </label>
                   </div>
                 </div>
               </div>
+              <div class="card-footer">
+                <button class="btn btn-primary">Submit</button>
+              </div>
             </div>
-                    </div>
-                </section>
-                
-            </div>
-           
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</div>
