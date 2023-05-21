1f3da1ca175415929b9390cfc05962a3bed6ebfa
Lucas Rocha <wltopos@gmail.com>
2023-05-21 20:46:16 -0300

    criar politicas

---
+++ politicas.php
@@ -0,0 +1,318 @@
+<!doctype html>
+<html class="h-100" lang="en">
+
+  <head>
+      <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="180x180" href="site/img/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="site/img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="site/img/favicon-16x16.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="site/.t/img/favicon.png">
+  <meta name="generator" content="Eleventy v2.0.0">
+  <title>WL TOPOS</title>
+  <link rel="stylesheet" href="site/css/theme.min.css">
+  
+  <!--META TAGS-->
+  <meta name="description" content="A WL Topos é uma empresa de tecnologia especializada em Inteligência Artificial (IA), Big Data, Cibersegurança, Desenvolvimento de Software, Robótica e Assistência Técnica. Conheça nossos serviços e soluções inovadoras.">
+<meta name="keywords" content="WL Topos, Tecnologia, Inteligência Artificial, IA, Big Data, Cibersegurança, Desenvolvimento de Software, Robótica, Assistência Técnica, Equipamentos Eletrônicos">
+<meta name="author" content="WL Topos">
+<meta name="robots" content="index, follow">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta property="og:title" content="WL Topos - Tecnologia em IA, Big Data, Cibersegurança, Desenvolvimento de Software e Robótica">
+<meta property="og:type" content="website">
+<meta property="og:url" content="http://www.wltopos.com">
+<meta property="og:image" content="http://www.wltopos.com/images/logo.png">
+<meta property="og:description" content="Conheça a WL Topos, empresa de tecnologia especializada em Inteligência Artificial (IA), Big Data, Cibersegurança, Desenvolvimento de Software, Robótica e Assistência Técnica.">
+<meta property="og:site_name" content="WL Topos">
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@toposwl">
+<meta name="twitter:creator" content="@toposwl">
+<meta name="twitter:title" content="WL Topos - Tecnologia em IA, Big Data, Cibersegurança, Desenvolvimento de Software e Robótica">
+<meta name="twitter:description" content="Conheça a WL Topos, empresa de tecnologia especializada em Inteligência Artificial (IA), Big Data, Cibersegurança, Desenvolvimento de Software, Robótica e Assistência Técnica.">
+<meta name="twitter:image" content="http://www.wltopos.com/images/logo.png">
+
+   <style>
+
+/* inter-300 - latin */
+@font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: local(''),
+       url('/site/fonts/inter-v12-latin-300.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('/site/fonts/inter-v12-latin-300.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+
+@font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 500;
+  font-display: swap;
+  src: local(''),
+       url('/site/fonts/inter-v12-latin-500.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('/site/fonts/inter-v12-latin-500.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+@font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 700;
+  font-display: swap;
+  src: local(''),
+       url('/site/fonts/inter-v12-latin-700.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('/site/fonts/inter-v12-latin-700.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+::-webkit-scrollbar {
+  width: 10px;
+  height: 10px;
+  background-color: #f5f5f5;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #aaa;
+  border-radius: 10px;
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: #999;
+}
+
+::-webkit-scrollbar-track {
+  background-color: #f5f5f5;
+}
+</style>
+
+  </head>
+
+  <body data-bs-spy="scroll" data-bs-target="#navScroll">
+
+    <nav id="navScroll" class="navbar navbar-expand-lg navbar-light fixed-top" tabindex="0">
+  <div class="container">
+    <a class="navbar-brand pe-4 fs-4" href="site//">
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-layers-half" viewbox="0 0 16 16">
+    <path d="M8.235 1.559a.5.5 0 0 0-.47 0l-7.5 4a.5.5 0 0 0 0 .882L3.188 8 .264 9.559a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882L12.813 8l2.922-1.559a.5.5 0 0 0 0-.882l-7.5-4zM8 9.433 1.562 6 8 2.567 14.438 6 8 9.433z"/>
+  </svg>
+  <span class="ms-1 fw-bolder">WL Topos</span>
+</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+    <a class="nav-link" href="#" aria-label="Brings you to the frontpage">
+      Serviços
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">
+      Quem somos
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">
+      Numeros
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">
+      Galeria
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">
+      Trabalhe conosco
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">
+      Depoimentos
+    </a>
+  </li>
+
+    </ul>
+      <a href="https://dev.wltopos.com/" data-splitbee-event="Click Download" aria-label="Download this template" class="link-dark pb-1 link-fancy me-2">
+        Acesse o sistema       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download ms-1" viewBox="0 0 16 16">
+                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+              </svg>
+      </a>
+
+    </div>
+</div>
+</nav>
+
+    <main>
+      <div class="w-100 overflow-hidden bg-gray-100" id="top">
+  
+<div class="container position-relative">
+  <div class="col-12 col-lg-8 mt-0 h-100 position-absolute top-0 end-0 bg-cover" data-aos="fade-left" style="background-image: url(site/img/webp/interior11.svg);">
+    
+  </div>
+    <div class="row">
+
+<div class="col-lg-7 py-vh-6 position-relative" data-aos="fade-right">
+  <h1 class="display-1 fw-bold mt-5">Soluções para Pequenos Negócios</h1>
+  <p class="lead">Obtenha soluções de tecnologia personalizadas para o seu pequeno negócio e suporte para seus dispositivos digitais com a equipe especializada da WL Topos. Inicie agora e transforme seu futuro em algo incrível!</p>
+  <a href="#" class="btn btn-dark btn-xl shadow me-3 rounded-0 my-5">Saiba mais</a>
+</div>
+
+
+
+</div>
+</div>
+
+</div>
+
+<div class="row d-flex align-items-start mt-5 border-top pt-5" data-aos="fade">
+        <div class="col-12 col-lg-8">
+          <span class="text-secondary">1.0</span>
+              <h2>Introduction</h2>
+              <p class="lead">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+                gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.</p>
+          <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+            ipsum dolor sit amet. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+        </div>
+        <div class="col-12 col-lg-3 offset-lg-1 bg-light p-5 mt-5">
+          <h3 class="h6">tl;dr</h3>
+          <p class="text-secondary">
+            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet
+          </p>
+        </div>
+      </div>
+
+<div class="container py-vh-3 border-top" data-aos="fade" data-aos-delay="200">
+  <div class="row d-flex justify-content-center">
+    <div class="col-12 col-lg-8 text-center">
+      <h3 class="fs-2 fw-light">Fique por dentro das novidades<span class="fw-bold">
+          mensal e gratuitas</span>!
+    </h3></div>
+    <div class="col-12 col-lg-8 text-center">
+      <div class="row">
+        <div class="grouped-inputs border bg-light p-2">
+  <div class="row">
+    <div class="col">
+      <form class="form-floating">
+        <input type="email" class="form-control p-3" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">E-mail</label>
+      </form>
+    </div>
+    <div class="col-auto">
+      <a href="#" class="btn btn-dark py-3 px-5">Inscreva-se</a>
+    </div>
+  </div>
+</div>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+    </main>
+
+    <footer>
+  <div class="container small border-top">
+    <div class="row py-5 d-flex justify-content-between">
+
+<div class="col-12 col-lg-6 col-xl-3 border-end p-5">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-layers-half" viewbox="0 0 16 16">
+    <path d="M8.235 1.559a.5.5 0 0 0-.47 0l-7.5 4a.5.5 0 0 0 0 .882L3.188 8 .264 9.559a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882L12.813 8l2.922-1.559a.5.5 0 0 0 0-.882l-7.5-4zM8 9.433 1.562 6 8 2.567 14.438 6 8 9.433z"/>
+  </svg>
+  <address class="text-secondary mt-3">
+    <strong>WL TOPOS, ME.</strong><br>
+    Rua Arthur Santos, 147 - Mares<br>
+    Salvador/BA, CEP 40445-090<br>
+    <abbr title="Phone">Fone:</abbr>
+    (71) 3733-3591
+  </address>
+</div>
+<div class="col-12 col-lg-6 col-xl-3 border-end p-5">
+  <h3 class="h6 mb-3">Empresa</h3>
+  <ul class="nav flex-column">
+    <li class="nav-item">
+      <a class="nav-link link-secondary ps-0" aria-current="page" href="#">Quem somos</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link link-secondary ps-0" href="#">Nossa Missão</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link link-secondary ps-0" href="#">Nossa Visão</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link link-secondary ps-0" href="#">Nossos Valores</a>
+    </li>
+  </ul>
+</div>
+<div class="col-12 col-lg-6 col-xl-3 border-end p-5">
+  <h3 class="h6 mb-3">Produos</h3>
+    <ul class="nav flex-column">
+    <li class="nav-item">
+      <a class="nav-link link-secondary ps-0" aria-current="page" href="#">Inteligência artificial (IA)</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link link-secondary ps-0" href="#">Big Data</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link link-secondary ps-0" href="#">Cibersegurança</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link link-secondary ps-0" href="#">Desenvolvimento de software</a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link link-secondary ps-0" href="#">Robótica</a>
+    </li>
+  </ul>
+</div>
+<div class="col-12 col-lg-6 col-xl-3 p-5">
+  <h3 class="h6 mb-3">Links Uteis</h3>
+  <ul class="nav flex-column">
+    <li class="nav-item">
+      <a class="nav-link link-secondary ps-0" aria-current="page" href="site/404.html">Planos</a>
+      <a class="nav-link link-secondary ps-0" aria-current="page" href="site/register.html">Pagina de cadastro</a>
+            <a class="nav-link link-secondary ps-0" aria-current="page" href="site/content.html">Blog</a>
+    </li>
+  </ul>
+</div>
+</div>
+</div>
+
+<div class="container text-center py-3 small">Made by <a href="https://wltopos.com" class="link-fancy" target="_blank">WL TOPOS</a>
+</div>
+</footer>
+
+    <script src="site/js/bootstrap.bundle.min.js"></script>
+<script src="site/js/aos.js"></script>
+ <script>
+ AOS.init({
+   duration: 800, // values from 0 to 3000, with step 50ms
+ });
+ </script>
+
+ <script>
+  let scrollpos = window.scrollY
+  const header = document.querySelector(".navbar")
+  const header_height = header.offsetHeight
+
+  const add_class_on_scroll = () => header.classList.add("scrolled", "shadow-sm")
+  const remove_class_on_scroll = () => header.classList.remove("scrolled", "shadow-sm")
+
+  window.addEventListener('scroll', function() {
+    scrollpos = window.scrollY;
+
+    if (scrollpos >= header_height) { add_class_on_scroll() }
+    else { remove_class_on_scroll() }
+
+    console.log(scrollpos)
+  })
+</script>
+
+  </body>
+</html>
