8d39b6624d5941d0fa1c7f3cdede6f513c2396a9
Lucas Rocha <wltopos@gmail.com>
2023-06-04 20:22:02 -0300

    Update Setdb_model.php

--- sistema_dev/application/models/Setdb_model.php
+++ sistema_dev/application/models/Setdb_model.php
@@ -94,7 +94,7 @@ class Setdb_model extends CI_Model
 
         $query = $this->db_empresa->get();
 
-        $result =  $query->row();
+        $result =  $query->num_rows();
         return $result;
     }
 
