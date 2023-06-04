3f60e7f56d37fb711873a8fc05036428c46449e3
Lucas Rocha <wltopos@gmail.com>
2023-06-04 08:43:53 -0300

    Update Settings.php

--- sistema_dev/application/controllers/Settings.php
+++ sistema_dev/application/controllers/Settings.php
@@ -55,7 +55,7 @@ class Settings extends MY_Controller
             }
 
             if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vProduto')) {
-                $v = '<a style="margin-right: 1%" href="' . site_url() . "settings/visualizar/".$this->data['titlo'] ."/" . $settings->{"id_estoque_".$this->data['titulo'] } . '" class="btn-nwe" title="Visualizar Setting"><i class="bx bx-show bx-xs"></i></a>  ';
+                $v = '<a style="margin-right: 1%" href="' . site_url() . "settings/visualizar/".$this->data['id'] ."/" . $settings->{"id_estoque_".$this->data['id'] } . '" class="btn-nwe" title="Visualizar Setting"><i class="bx bx-show bx-xs"></i></a>  ';
             }
             if ($this->permission->checkPermission($this->session->userdata('permissao'), 'eProduto')) {
                 $e = '<a style="margin-right: 1%" href="' . site_url() . "settings/editar/".$this->data['titulo'] ."/" . $settings->{"id_estoque_".$this->data['id']} . '" class="btn-nwe3" title="Editar Setting"><i class="bx bx-edit bx-xs"></i></a>';
