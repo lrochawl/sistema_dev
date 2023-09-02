673d023496350d809bd1b5cef8a5e87411da7fe6
Lucas Rocha <49371821+lrochawl@users.noreply.github.com>
2023-09-01 22:18:59 -0300

    update

--- sistema_dev/application/views/produtos/editarProduto.php
+++ sistema_dev/application/views/produtos/editarProduto.php
@@ -298,9 +298,10 @@
                                 <span class="button__icon"><i class="bx bx-sync"></i></span><span class="button__text2">Atualizar</span></button>
                             <a href="<?php echo base_url() ?>index.php/produtos" id="" class="button btn btn-mini btn-warning">
                                 <span class="button__icon"><i class="bx bx-undo"></i></span><span class="button__text2">Voltar</span></a>
-                            <a href="#" id="botaoGerarCodigo" class="button btn btn-mini btn-info">
+                            <!-- <a href="#" id="botaoGerarCodigo" class="button btn btn-mini btn-info">
                                 <span class="button__icon"><i class="bx bx-barcode-reader"></i></span><span class="button__text2">Gerar codigo</span></a>
-                        </div>
+                        -->
+                            </div> 
                     </div>
                 </div>
             </form>
