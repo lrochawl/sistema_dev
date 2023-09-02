5cb9cfbf2b03cae65f6ddadd5b02fdaa3eceb843
Lucas Rocha <49371821+lrochawl@users.noreply.github.com>
2023-09-02 16:50:55 -0300

    Update adicionarProduto.php

--- sistema_dev/application/views/produtos/adicionarProduto.php
+++ sistema_dev/application/views/produtos/adicionarProduto.php
@@ -519,7 +519,7 @@ $(document).ready(function() {
     const subcategoriaSelecionada = subcategoriaSelect.val();
     const marcaSelecionada = marcaSelect.find(":selected").text();
   //  const descricao = descricaoInput.val();
-    const lastID = lastId.val();
+    const lastID = lastId.val()+1;
 
     // Gerando o código
     if(categoriaSelecionada != null && subcategoriaSelecionada != null && marcaSelecionada != null && descricao  != null){
@@ -527,7 +527,7 @@ $(document).ready(function() {
       subcategoriaSelecionada +
       marcaSelecionada.slice(0, 3).toUpperCase() +
       //descricao.slice(0, 3).toUpperCase()+
-      (lastID+1);
+      lastID;
 
       // Removendo caracteres especiais do código
     codigo = removerCaracteresEspeciais(codigo);
