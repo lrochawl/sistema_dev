494e4260081bced6e869736782449f915ece8740
Lucas Rocha <49371821+lrochawl@users.noreply.github.com>
2023-09-03 09:10:32 -0300

    Update Produtos.php

--- sistema_dev/application/controllers/Produtos.php
+++ sistema_dev/application/controllers/Produtos.php
@@ -475,7 +475,7 @@ class Produtos extends MY_Controller
         if (!$this->upload->do_upload()) {
             $error = ['error' => $this->upload->display_errors()];
 
-             $this->session->set_flashdata('error', "Erro ao fazer upload do arquivo, verifique se a extensão do arquivo é permitida. $erro");
+             $this->session->set_flashdata('error', "Erro ao fazer upload do arquivo, verifique se a extensão do arquivo é permitida. $error");
             // redirect(site_url('settings/'));
 
             try {
