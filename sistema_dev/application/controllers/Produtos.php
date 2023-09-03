8df45b2e1f4d83f13658854012dfbda7970b4e46
Lucas Rocha <49371821+lrochawl@users.noreply.github.com>
2023-09-03 09:02:21 -0300

    Update Produtos.php

--- sistema_dev/application/controllers/Produtos.php
+++ sistema_dev/application/controllers/Produtos.php
@@ -459,7 +459,7 @@ class Produtos extends MY_Controller
 
 
         $config['upload_path'] = './assets/uploads/' . $this->session->userdata('dbEmpresa') . "/" . "imagemProdutos/";
-        $config['allowed_types'] = 'jpg|jpeg|png|JPG|JPEG|PNG';
+        $config['allowed_types'] = 'jpg|jpeg|png|JPG|JPEG|PNG|webp';
         $config['max_size'] = 0;
         $config['max_width'] = 0;
         $config['max_height'] = 0;
